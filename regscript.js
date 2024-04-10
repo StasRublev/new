282df9ce9085c7db8b0a40e0792a0bf0b1e14f91
StasRublev <165238009+StasRublev@users.noreply.github.com>
2024-04-10 21:09:20 +0500

    Add files via upload

---
+++ regscript.js
@@ -0,0 +1,40 @@
+document.getElementById('2to1').addEventListener('click', function() {
+    document.getElementById('2').classList.add('hidden');
+    document.getElementById('1').classList.remove('hidden');
+});
+document.getElementById('1to2').addEventListener('click', function() {
+    document.getElementById('1').classList.add('hidden');
+    document.getElementById('2').classList.remove('hidden');
+});
+document.getElementById('3to2').addEventListener('click', function() {
+    document.getElementById('3').classList.add('hidden');
+    document.getElementById('2').classList.remove('hidden');
+});
+document.getElementById('2to3').addEventListener('click', function() {
+    document.getElementById('2').classList.add('hidden');
+    document.getElementById('3').classList.remove('hidden');
+});
+document.getElementById('4to3').addEventListener('click', function() {
+    document.getElementById('4').classList.add('hidden');
+    document.getElementById('3').classList.remove('hidden');
+});
+document.getElementById('3to4').addEventListener('click', function() {
+    document.getElementById('3').classList.add('hidden');
+    document.getElementById('4').classList.remove('hidden');
+});
+document.getElementById('5to4').addEventListener('click', function() {
+    document.getElementById('5').classList.add('hidden');
+    document.getElementById('4').classList.remove('hidden');
+});
+document.getElementById('4to5').addEventListener('click', function() {
+    document.getElementById('4').classList.add('hidden');
+    document.getElementById('5').classList.remove('hidden');
+});
+document.getElementById('6to5').addEventListener('click', function() {
+    document.getElementById('6').classList.add('hidden');
+    document.getElementById('5').classList.remove('hidden');
+});
+document.getElementById('5to6').addEventListener('click', function() {
+    document.getElementById('5').classList.add('hidden');
+    document.getElementById('6').classList.remove('hidden');
+});
